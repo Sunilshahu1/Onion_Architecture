50fe9f7461b29b755f3650d37c892de492e22884
Sunil Shahu <61456130+Sunilshahu1@users.noreply.github.com>
2022-08-16 12:22:40 +0530

    Add files via upload

---
+++ StudentManagement/StudentManagement/Controller/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagement.Controller
+{
+    public class StudentController : Controller
+    {
+      
+
+
+    }
+}
